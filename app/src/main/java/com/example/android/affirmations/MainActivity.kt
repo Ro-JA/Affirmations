7789d79d5ec004c97fc66fa8817c6bf311fd9e84
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-12-03 12:30:05 +0900

    work

--- app/src/main/java/com/example/android/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/android/affirmations/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.android.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.affirmations.adapter.ItemAdapter
 import com.example.android.affirmations.data.Datasource
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.text_view)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        val myDataset = Datasource().loadAffirmations()
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
