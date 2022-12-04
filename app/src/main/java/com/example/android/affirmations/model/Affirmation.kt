dd330e53d3057153ea6e1cc52662a489222f4204
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-12-04 14:26:34 +0900

    add image and work

--- app/src/main/java/com/example/android/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/android/affirmations/model/Affirmation.kt
@@ -1,5 +1,11 @@
 package com.example.android.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 //создали дата класс высказывания который принимает строковый ресурс с высказываниями
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(
+    @StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int
+) {
 
 }
